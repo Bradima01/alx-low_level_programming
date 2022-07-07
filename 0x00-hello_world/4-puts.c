3975a4472415049d9dd8b20c4054a3d160d36cdd
Bryan Dima <108438496+Bradima01@users.noreply.github.com>
2022-07-07 10:40:21 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * return: always 0 (success)
- */
-int main(void)
+#!/bin/bash
 {
   puts("/"programming is like building a multilingual puzzle");
 	return (0);
