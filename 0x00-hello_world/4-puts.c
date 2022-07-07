70d545e6b4b9b136d878b45e9dfa56e987cca4ba
Bradima01 <Bradima01>
2022-07-07 00:29:06 -0700

    my network

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-puts "Programming is like building a multilingual puzzle"
-#!/bin/bash
+#include <stdo.h>
+/**
+ * main - Entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{	puts("/"programming s like building a multilingual puzzle");
+}	return (0);
